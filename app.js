4f6d59b3e999abccccc734c93caf9d1678aa93de
TheodoreFrazier <theo.frazier@icloud.com>
2022-01-18 21:34:54 -0500

    App is listening on port 7777

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const { response } = require("express");
 const express = require("express");
 
+const app = express();
+
 console.log(express)
+
+app.get("/", (request, reponse) => {
+    response.send("Hello world")
+})
+
+//
+
+app.listen(7777)
