d6321577b6b2064a621e929340aa9b1eeca75ae7
TheodoreFrazier <theo.frazier@icloud.com>
2022-01-19 14:10:20 -0500

    server page crearted - nodemon is not working

---
+++ server.js
@@ -0,0 +1,16 @@
+// CREATION OF THE SERVER
+
+// CREATE DEPENDENCIES 
+
+const app = require("./app.js");
+
+// CONFIGURE dotenv
+
+require("dotenv").config();
+const PORT = process.env.PORT
+
+// LISTENER
+
+app.listen(PORT, () => {
+    console.log(` Now listening on port ${PORT}`);
+})
