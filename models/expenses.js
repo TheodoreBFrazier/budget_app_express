2080760aa1f54d2eeaef83c14f7c79ef9e190a28
TheodoreBFrazier <theo.frazier@icloud.com>
2022-01-23 14:18:13 -0500

    created model of expenses

---
+++ models/expenses.js
@@ -0,0 +1,61 @@
+module.exports = [   
+    {
+        expense_name: "Ride from NY party",
+        amount: 14.00,
+        date: "January 1",
+        from: "Lyft",
+        category: "tranportation"
+    },
+    {
+        expense_name: "Credit card payment",
+        amount: 25.00,
+        date: "January 1",
+        from: "Discover",
+        category: "debt reduction"
+    },
+    {
+        expense_name: "Refill coffee card",
+        amount: 50.00,
+        date: "January 4",
+        from: "Starbucks Coffee",
+        category: "food & drink"
+
+    },
+    {
+        expense_name: "Refill coffee card",
+        amount: 50.00,
+        date: "January 4",
+        from: "Starbucks Coffee",
+        category: "food & drink"
+
+    },
+    {
+        expense_name: "Cell Phone",
+        amount: 75.00,
+        date: "January 8",
+        from: "T-mobile",
+        category: "telecomm"
+    },
+    {
+        expense_name: "Cell Phone",
+        amount: 75.00,
+        date: "January 8",
+        from: "T-mobile",
+        category: "telecomm"
+    },
+    {
+        expense_name: "New sweater",
+        amount: 200.00,
+        date: "January 16",
+        from: "Brooks Brothers",
+        category: "clothing"
+    },
+    {
+        expense_name: "Movie night",
+        amount: 35.00,
+        date: "January 23",
+        from: "AMC",
+        category: "leisure",
+    }
+
+];
