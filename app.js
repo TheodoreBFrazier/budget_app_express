3b9ab285a4f10401d76cb4ef929ab8d3b4f313a3
TheodoreBFrazier <theo.frazier@icloud.com>
2022-01-23 19:11:09 -0500

    nodemon working again

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 const express = require("express");
 
+// Make index of all the expenses
+const expenses = require("./models/expenses.js")
+
 const app = express();
 
 console.log(express)
 
 
 app.get("/", (request, response) => {
-    response.send("Welcome to the budgeting app!")
+    response.send("Welcome to the budgeting app!!🤪");
 })
 
 //Route for index 
 
-app.get("/index", (request, response) => {
-    response.send("Here is the index.")
+app.get("/expenses", (request, response) => {
+    response.send(expenses); //changing to req.params
 })
 
 //Route for new expense
 
 app.get("/new", (request, response) => {
-    response.send("Add a new expense")
+    response.send("Add a new expense");
 })
 
 //Route for show 
 
 app.get("/show", (request, response) => {
-    response.send("Show expenses!")
+    response.send("Show expenses!");
 })
 
 
 
 app.get("/edit", (request, response) => {
-    response.send("Edit function here")
+    response.send("Edit function here");
 })
 
 // App listen function 
