e4474602c02eba8f8aaba4af628bad46e2fef818
TheodoreBFrazier <theo.frazier@icloud.com>
2022-01-25 12:44:03 -0500

    Controllers in process of being set up - temporarily broke server but now it's up an running again 💪

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -2,8 +2,21 @@ const express = require("express");
 const expensesRoute = express.Router();
 const expenseArray = require("../models/expenses.js");
 
+// Expenses
 expensesRoute.get("/", (request, response) => {
     response.json(expenseArray) //We are sending json instead of string, so use res.json
+})
+
+//Route for index 
+
+
+expensesRoute.get("/transactions/:id", (request, response) => {
+    const { id } = request.params;
+    if (expenseArray[id]) {
+        response.send(expenseArray[id]);
+    } else {
+        response.send("Error no expense at index: 🤦🏾‍♂️ " + id ) ;
+    }
 })
 
 module.exports = expensesRoute;
--- app.js
+++ app.js
@@ -1,77 +1,26 @@
 const express = require("express");
 
-// Make index of all the expenses
-const expenses = require("./models/expenses.js")
-
 //Configuration
 const app = express();
 
-console.log(express)
 
 // Using expenses as base of the routes
+const expensesController = require("./controllers/Controller.js");
 
-const expensesController = require("./Controllers/Controller.js");
-app.use("/expenses", expensesController);
-
-//404 Page 
-
-app.get("*", (request, response) => {
-    response.status(404).json({ error: "Page not found"});
-})
-
-//Home page
 
 app.get("/", (request, response) => {
     response.send(`
     <h1>Welcome to the budgeting app!! 😮</h1>`);
 })
 
-//Route for index 
+app.use("/transactions", expensesController)
 
-app.get("/transactions", (request, response) => {
-    response.send(expenses); //changing to req.params
-})
+//404 Page 
 
-app.get("/transactions/:id", (request, response) => {
-    const { id } = request.params;
-    if (expenses[id]) {
-        response.send(expenses[id]);
-    } else {
-        response.send("Error no expense at index: 🤦🏾‍♂️ " + id ) ;
-    }
+app.get("*", (request, response) => {
+   response.status(404).json({ error: "Page not found"});
 })
 
 
-
-
-
-
-//Route for new expense
-
-app.get("/new", (request, response) => {
-    response.send("Add a new expense");
-})
-
-//Route for show 
-
-app.get("/show", (request, response) => {
-    response.send("Show expenses!");
-})
-
-
-
-app.get("/edit", (request, response) => {
-    response.send("Edit function here");
-})
-
-// App listen function 
-
-//app.listen(7777, () => {
-//console.log("Listening for request on port 7777 😀😂");
-//})
-
-
-
-// EXPORTING APP
 
 module.exports = app;
