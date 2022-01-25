c2f81256578c659de28987d505bdc8122012fc7b
TheodoreBFrazier <theo.frazier@icloud.com>
2022-01-25 15:34:04 -0500

    curl request working

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const expensesRoute = express.Router();
 const expenseArray = require("../models/expenses.js");
@@ -7,15 +8,22 @@ expensesRoute.get("/", (request, response) => {
     response.json(expenseArray) //We are sending json instead of string, so use res.json
 })
 
-//Route for index 
+//Route for index ie. transactions/1
 
 expensesRoute.get("/:id", (request, response) => {
     const { id } = request.params;
     if (expenseArray[id]) {
         response.send(expenseArray[id]);
     } else {
-        response.send("Error no expense at index: 🤦🏾‍♂️ " + id ) ;
+        response.send("Error no expense at index: 🤦🏾‍♂️ " + id);
     }
+})
+
+//Route to create a new transaction
+
+expensesRoute.post("/", (request, response) => { //post allows us to pass date through response body
+    expenseArray.push(request.body); //expenseArray is stored in memory - pushing HTML for data into it
+    response.json(expenseArray[expenseArray.length - 1])
 })
 
 module.exports = expensesRoute;
